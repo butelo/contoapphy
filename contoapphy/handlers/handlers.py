d8c96ac4964a7dd41d9002d44a0c2b50617006e4
Xes Garcia <xesgarcia@gmail.com>
2012-05-21 18:58:00 +0200

    algo

--- contoapphy/handlers/handlers.py
+++ contoapphy/handlers/handlers.py
@@ -1,1 +1,9 @@
+import xml.sax.handler
+class ArticleHandler(ContentHandler):
+	"""
+	A handler to deal with articles in XML
+	"""
+
+	def startElement(self, name, attrs):
+		print "Start element:", name
 
